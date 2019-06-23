e62df98dba3eb5d8b11ef803911c9002f038ff41
autopp <autopp.inc@gmail.com>
2019-06-23 23:07:58 +0900

    feat: show by using mybatis

--- src/main/java/tree/Main.java
+++ src/main/java/tree/Main.java
@@ -1,7 +1,27 @@
 package tree;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("hello world");
-  }
+    public static void main(String[] args) throws IOException {
+        try (InputStream in = Main.class.getResourceAsStream("/mybatis-config.xml")) {
+        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(in);
+
+            try (SqlSession session = factory.openSession()) {
+                List<Map<String, Object>> result = session.selectList("tree.selectNaive");
+
+                result.forEach(row -> {
+                    System.out.println(row);
+                });
+            }
+        }
+    }
 }
