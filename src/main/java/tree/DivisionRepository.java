ff8e4f90b34714355abe5cb813895fad153e94f3
autopp <autopp.inc@gmail.com>
2019-06-26 09:41:05 +0900

    change: use abstract class

--- src/main/java/tree/DivisionRepository.java
+++ src/main/java/tree/DivisionRepository.java
@@ -1,8 +1,14 @@
 package tree;
 
 import java.util.List;
+import org.apache.ibatis.session.SqlSessionFactory;
 
-public interface DivisionRepository {
+public abstract class DivisionRepository {
+    private final SqlSessionFactory factory;
+    public DivisionRepository(SqlSessionFactory factory) {
+        this.factory = factory;
+    };
+
     public void addDivision(long id, String name, long parentId);
     public Division getParentOf(long id);
     public List<Division> getAncestorsOf(long id);
