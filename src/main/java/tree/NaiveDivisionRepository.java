8e93314eabed6aba290ad34b97dd0d7c00cd0580
autopp <autopp.inc@gmail.com>
2019-06-28 22:53:01 +0900

    feat: add naive tree implementation

---
+++ src/main/java/tree/NaiveDivisionRepository.java
@@ -0,0 +1,25 @@
+package tree;
+
+import java.util.List;
+
+public class NaiveDivisionRepository {
+    public void addDivision(long id, String name, long parentId) {
+        throw new UnsupportedOperationException("not supported");
+    }
+
+    public Division getParentOf(long id) {
+        throw new UnsupportedOperationException("not supported");
+    }
+
+    public List<Division> getAncestorsOf(long id) {
+        throw new UnsupportedOperationException("not supported");
+    }
+
+    public List<Division> getChildsOf(long id) {
+        throw new UnsupportedOperationException("not supported");
+    }
+
+    public List<Division> getDescendantsOf(long id) {
+        throw new UnsupportedOperationException("not supported");
+    }
+}
