7eb0081dff0a4b089c3a4a6bb58e484d65567d6c
autopp <autopp.inc@gmail.com>
2019-06-29 09:12:39 +0900

    feat: impl NaiveDivisionRepository::addDivision

--- src/main/java/tree/NaiveDivisionRepository.java
+++ src/main/java/tree/NaiveDivisionRepository.java
@@ -3,6 +3,18 @@ package tree;
 import java.util.List;
 
 public class NaiveDivisionRepository {
+    public class NaiveDivision {
+        public long id;
+        public String name;
+        public long parentId;
+
+        public NaiveDivision(long id, String name, long parentId) {
+            this.id = id;
+            this.name = name;
+            this.parentId = parentId;
+        }
+    }
+
     public void addDivision(long id, String name, long parentId) {
         throw new UnsupportedOperationException("not supported");
     }
