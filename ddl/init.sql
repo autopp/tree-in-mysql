b57ed5108c002b77d2b0ba2044f14bd980f1b2ef
autopp <autopp.inc@gmail.com>
2019-07-03 22:28:52 +0900

    feat: insert root node

--- ddl/init.sql
+++ ddl/init.sql
@@ -6,3 +6,5 @@ CREATE TABLE division_naive (
   parent_id INTEGER,
   FOREIGN KEY(parent_id) REFERENCES division_naive (id)
 );
+
+INSERT INTO division_naive (id, name, parent_id) VALUES (0, "root", null);
