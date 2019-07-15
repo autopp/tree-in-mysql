f0e16b5f3ae170a7f991b5443d62a39bbb3a2f53
autopp <autopp.inc@gmail.com>
2019-07-15 23:05:08 +0900

    test: add test for getParentOf

--- src/test/java/tree/NaiveDivisionRepositoryTest.java
+++ src/test/java/tree/NaiveDivisionRepositoryTest.java
@@ -1,5 +1,11 @@
 package tree;
 
+import java.io.InputStream;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
 import com.ninja_squad.dbsetup.DbSetup;
 import com.ninja_squad.dbsetup.DbSetupTracker;
 import com.ninja_squad.dbsetup.Operations;
@@ -10,6 +16,7 @@ import com.ninja_squad.dbsetup.operation.Operation;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
@@ -19,15 +26,32 @@ class NaiveDivisionRepositoryTest {
     static final String PASSWORD = "";
 
     private static final DbSetupTracker dbSetupTracker = new DbSetupTracker();
+    static SqlSessionFactory factory;
+    NaiveDivisionRepository repository;
+
+    @BeforeAll
+    static void setupOnce() {
+        InputStream in = Main.class.getResourceAsStream("/mybatis-config.xml");
+        factory = new SqlSessionFactoryBuilder().build(in);
+    }
 
     @BeforeEach
     void setup() {
         Operation operation = sequenceOf(
+            truncate("division_naive"),
             insertInto("division_naive").columns("id", "parent_id")
                 .values(1, null).values(2, 1).values(3, 1).values(4, 3).values(5, 3).build()
         );
         Destination dest = new DriverManagerDestination(URL, USER, PASSWORD);
         DbSetup dbSetup = new DbSetup(dest, operation);
         dbSetupTracker.launchIfNecessary(dbSetup);
+
+        repository = new NaiveDivisionRepository(factory);
+    }
+
+    @Test
+    void getParentOf() {
+        dbSetupTracker.skipNextLaunch();
+        assertThat(repository.getParentOf(2).id, is(1));
     }
 }
