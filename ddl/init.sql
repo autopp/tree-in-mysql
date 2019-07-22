df1e5cf31418b9cc7eb2351b976c2002dc517cae
autopp <autopp.inc@gmail.com>
2019-07-22 09:43:27 +0900

    feat: add division_path table

--- ddl/init.sql
+++ ddl/init.sql
@@ -1,7 +1,13 @@
 DROP TABLE IF EXISTS division_naive;
+DROP TABLE IF EXISTS division_path;
 
 CREATE TABLE division_naive (
   id INTEGER NOT NULL PRIMARY KEY,
   parent_id INTEGER,
   FOREIGN KEY(parent_id) REFERENCES division_naive (id)
+);
+
+CREATE TABLE division_path (
+  id INTEGER NOT NULL PRIMARY KEY,
+  path TEXT NOT NULL
 );
