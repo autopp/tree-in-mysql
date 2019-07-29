f4fd7f3d1135be2763309e3304d8ae59a1ef24a3
autopp <autopp.inc@gmail.com>
2019-07-29 23:50:13 +0900

    perf: calc parent in query

--- src/main/java/tree/PathDivisionRepository.java
+++ src/main/java/tree/PathDivisionRepository.java
@@ -36,9 +36,7 @@ public class PathDivisionRepository extends DivisionRepository {
 
     public Integer getParentOf(int id) {
         try (SqlSession session = factory.openSession()) {
-            String path = session.selectOne("tree.PathDivisionMapper.getPathOf", id);
-            String[] ids = path.split("/");
-            return ids.length == 1 ? null : Integer.parseInt(ids[ids.length - 2]);
+            return session.selectOne("tree.PathDivisionMapper.getParentOf", new PathDivision(id, null));
         }
     }
 
